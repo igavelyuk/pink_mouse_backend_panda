4a15c7ef47630068dbbd6030b0de7b7b3a5a13ef
igavelyuk <gavelyukworking@gmail.com>
2020-08-16 15:56:35 +0300

    using querystring update test

--- panda.js
+++ panda.js
@@ -4,19 +4,32 @@ const { parse } = require('querystring');
 const server = http.createServer(function(request, response) {
   console.dir(request.param)
 
-  if (request.method == 'POST') {
-    console.log('POST')
-    var body = ''
-    request.on('data', function(data) {
-      body += data
-      console.log('Partial body: ' + body)
-    })
-    request.on('end', function() {
-      console.log('Body: ' + body)
-      response.writeHead(200, {'Content-Type': 'text/html'})
-      response.end('post received')
-    })
-  }
+  // if (request.method == 'POST') {
+  //   console.log('POST')
+  //   var body = ''
+  //   request.on('data', function(data) {
+  //     body += data
+  //     console.log('Partial body: ' + body)
+  //   })
+  //   request.on('end', function() {
+  //     console.log('Body: ' + body)
+  //     response.writeHead(200, {'Content-Type': 'text/html'})
+  //     response.end('post received')
+  //   })
+  // }
+
+  if (req.method === 'POST') {
+    let body = '';
+    req.on('data', chunk => {
+        body += chunk.toString();
+    });
+    req.on('end', () => {
+        console.log(
+            parse(body)
+        );
+        res.end('ok');
+    });
+}
 })
 
 const port = 3000
